5707029d8076cceea6dfb5edb9b3247f92ec70e9
Karl Johannes <karl.frjo@gmail.com>
2025-04-13 09:56:10 +0100

    uploaded projects subpage and added global hyperlinks config file

---
+++ src/config.js
@@ -0,0 +1,13 @@
+const config = {
+    socialMedia: {
+        instagram: 'https://www.instagram.com/qmelectronics_soc/',
+        github: 'https://github.com/Queen-Mary-Electronics-Society',
+        linkedin: 'https://www.linkedin.com/company/queen-mary-electronics-society/'
+    },
+    contact: {
+        email: 'qmelectronicssociety@qmsu.org'
+    }
+};
+
+// Make config available globally
+window.siteConfig = config; 
