cc161c2ae153e5bbdd6c67dc85f525d7d8d22fe6
Karl Johannes <karl.frjo@gmail.com>
2025-04-24 10:02:22 +0100

    added dynamic links depending on local or deployment server

--- docs/js/navbar.js
+++ docs/js/navbar.js
@@ -1,16 +1,19 @@
 export function initializeNavbar() {
+    // Get the base URL from the config
+    const baseUrl = window.siteConfig?.baseUrl || '';
+    
     const navbarHTML = `
         <nav>
             <div class="logo">
-                <a href="index.html">
+                <a href="${baseUrl}/index.html">
                     <img src="../assets/qmul-logo.png" alt="QMES Logo">
                 </a>
             </div>
             <div class="nav-links">
                 <a href="#" id="nav-join-link" target="_blank" rel="noopener noreferrer">Join</a>
-                <a href="#" id="nav-about-link">About us</a>
-                <a href="projects.html">Projects</a>
-                <a href="contact.html">Contact us</a>
+                <a href="${baseUrl}/#about">About us</a>
+                <a href="${baseUrl}/projects.html">Projects</a>
+                <a href="${baseUrl}/contact.html">Contact us</a>
             </div>
         </nav>
     `;
